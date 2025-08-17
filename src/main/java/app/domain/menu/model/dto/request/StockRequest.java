a7252cdf04ea3dd001db344ac68592506d301c64
huniieee <sehoon981014@gmail.com>
2025-08-17 11:24:04 +0900

    test(store) domain/menu stock 감소 락 걸기 전 test
    1. 2개의 스레드로 번갈아가며 100번 재고를 감소 시키기
    2. 하지만 100개가 감소되지 않음
    3. 분산락 도입 필요
    
    #ref15

--- src/main/java/app/domain/menu/model/dto/request/StockRequest.java
+++ src/main/java/app/domain/menu/model/dto/request/StockRequest.java
@@ -1,10 +1,12 @@
 package app.domain.menu.model.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
+@AllArgsConstructor
 public class StockRequest {
 
     private UUID menuId;

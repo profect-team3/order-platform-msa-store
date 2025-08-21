f7fb5f4ee11657eddeb023dd4c8a3498b7c33389
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-08-21 18:35:26 +0900

    fix(store) @id 빼면안됨

--- src/main/java/app/domain/mongo/model/entity/StoreCollection.java
+++ src/main/java/app/domain/mongo/model/entity/StoreCollection.java
@@ -17,6 +17,8 @@ import lombok.Setter;
 @Document(collection = "stores")
 @CompoundIndex(name = "store_search_index", def = "{'categoryKeys': 1, 'regionName': 1, 'isActive': 1}")
 public class StoreCollection {
+    @Id
+    private String id;
 	private Long userId;
 	private String storeKey;
 	private String storeName;

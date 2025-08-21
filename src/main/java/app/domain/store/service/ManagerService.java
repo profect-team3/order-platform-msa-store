10f4734a63ed2fee1cf3a282bf23300106a7832d
huniieee <sehoon981014@gmail.com>
2025-08-21 13:05:48 +0900

    fix(store) api response 수정
    1. CustomerStoreService 수정
    2. ManagerService 수정

--- src/main/java/app/domain/store/service/ManagerService.java
+++ src/main/java/app/domain/store/service/ManagerService.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 import app.domain.store.client.ReviewClient;
 import app.domain.store.client.UserClient;
@@ -22,9 +24,11 @@ import app.global.apiPayload.PagedResponse;
 import app.global.apiPayload.code.status.ErrorStatus;
 import app.global.apiPayload.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ManagerService {
 
 	private final StoreRepository storeRepository;
@@ -43,16 +47,22 @@ public class ManagerService {
 			.orElseThrow(() -> new GeneralException(ErrorStatus.STORE_NOT_FOUND));
 
 		List<UUID> storeIds=List.of(store.getStoreId());
-		ApiResponse<List<ReviewClient.StoreReviewResponse>> getStoreListResponse=reviewClient.getStoreReviewAverage(storeIds);
-		if(!getStoreListResponse.isSuccess()){
+		ApiResponse<List<ReviewClient.StoreReviewResponse>> getStoreListResponse;
+		try{
+			getStoreListResponse=reviewClient.getStoreReviewAverage(storeIds);
+		} catch(HttpServerErrorException | HttpClientErrorException e){
+			log.error("Review Service Error: {}", e.getResponseBodyAsString());
 			throw new GeneralException(ErrorStatus._INTERNAL_SERVER_ERROR);
 		}
 
 		Double avgRating = getStoreListResponse.result().get(0).getAverage();
-		ApiResponse<GetUserInfoResponse> getUserInfoResponse = userClient.getUserInfo();
-		if(!getStoreListResponse.isSuccess()){
+		ApiResponse<GetUserInfoResponse> getUserInfoResponse;
+		try{
+			getUserInfoResponse = userClient.getUserInfo();
+		} catch (HttpServerErrorException | HttpClientErrorException e){
 			throw new GeneralException(ErrorStatus.USER_NOT_FOUND);
 		}
+
 		GetUserInfoResponse userInfo = getUserInfoResponse.result();
 
 		return GetStoreDetailResponse.from(store, userInfo.getUserId(), userInfo.getEmail(),
